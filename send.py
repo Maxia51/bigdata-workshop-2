e12ab0f962c9523821c35abceb427974b63df817
Maxence Margaine <maxence@MacBook-Pro-de-Maxence.local>
2019-12-13 14:20:20 +0100

    fix(python): remove useless comments

--- send.py
+++ send.py
@@ -1,28 +1,3 @@
-# from kafka import KafkaProducer
-# from kafka.errors import KafkaError
-
-# producer = KafkaProducer(security_protocol="PLAINTEXT",bootstrap_servers=['localhost:9092'], api_version = (0,10,2,0), request_timeout_ms=5000)
-
-# def on_send_success(record_metadata):
-#     print(record_metadata.topic)
-#     print(record_metadata.partition)
-#     print(record_metadata.offset)
-#     print("success")
-
-# def on_send_error(excp):
-#     print ("error")
-#     print (excp)
-
-#     print ('I am an errback', exc_info=excp)
-#     # handle exception
-
-# for _ in range(100):
-#     producer.send('test', b'some_message_bytes').add_callback(on_send_success).add_errback(on_send_error)
-# producer.flush()
-
-
-# Other Lib
-
 from pykafka import KafkaClient
 
 client = KafkaClient(hosts="127.0.0.1:9092")
